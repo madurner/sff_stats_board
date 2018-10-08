2625fdf640becdd5b2ca6be85dabfdb62efbfc89
maxdur <max.durner@gmail.com>
2018-10-08 14:41:29 +0200

    Update read_tables_from_url.py

--- read_tables_from_url.py
+++ read_tables_from_url.py
@@ -7,3 +7,5 @@ import pandas as pd
 
 url = r'https://www.basketball-bund.net/public/ergebnisDetails.jsp?type=1&spielplan_id=1828276&liga_id=26155&defaultview=1'
 tables = pd.read_html(url)
+
+#check http://srome.github.io/Parsing-HTML-Tables-in-Python-with-BeautifulSoup-and-pandas/
