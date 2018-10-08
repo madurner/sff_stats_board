41ace4c92292c772fa4b3f1cade4cce76b276572
maxdur <max.durner@gmail.com>
2018-10-08 14:39:10 +0200

    Create read_tables_from_url.py

---
+++ read_tables_from_url.py
@@ -0,0 +1,9 @@
+#import urllib2
+#response = urllib2.urlopen('https://www.basketball-bund.net/public/ergebnisDetails.jsp?type=1&spielplan_id=1828276&liga_id=26155&defaultview=1')
+#html = response.read()
+#from xml.etree.ElementTree import fromstring
+
+import pandas as pd
+
+url = r'https://www.basketball-bund.net/public/ergebnisDetails.jsp?type=1&spielplan_id=1828276&liga_id=26155&defaultview=1'
+tables = pd.read_html(url)
